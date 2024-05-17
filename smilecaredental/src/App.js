0661633c5902db4866f82d5975e0884ccecadd50
hoangduc120 <hoangduc05122003@gmail.com>
2024-05-17 20:55:34 +0700

    add bootstrap

---
+++ smilecaredental/src/App.js
@@ -0,0 +1,15 @@
+/* The following line can be included in your src/index.js or App.js file */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
