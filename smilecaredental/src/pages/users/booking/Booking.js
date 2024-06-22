55f1956594c7dadff43e8894fcd2a8c1ad534687
hoangduc120 <hoangduc05122003@gmail.com>
2024-06-22 21:46:03 +0700

    fix conflicts

--- smilecaredental/src/pages/users/booking/Booking.js
+++ smilecaredental/src/pages/users/booking/Booking.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import { useFormik } from "formik";
-import * as yup from "yup";
-import { Formik, Form, useFormik } from "formik";
+import { Formik, Form,useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import {
